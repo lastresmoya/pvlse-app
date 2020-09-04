7a4f579fb54a46c0f4e09ebb20c76564789da871
rob Moya <lastresmoyallc@gmail.com>
2020-09-04 16:31:26 -0600

    add build folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/customTheme.scss';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
