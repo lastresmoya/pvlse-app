d9a2f8013cb7efb4a90d496aa1076fc8b2554ab2
rob Moya <lastresmoyallc@gmail.com>
2020-09-17 15:22:58 -0600

    add db

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.scss'
+import { PrismaClient } from '@prisma/client';
 
-export default function Home() {
+export async function getStaticProps() {
+  const prisma = new PrismaClient();
+  const songs = await prisma.song.findMany({
+    include: { artist: true }
+  });
+
+  return {
+    props: {
+      songs
+    }
+  };
+}
+
+export default function Home(songs) {
+  console.log('data from db',songs)
   return (
     <Layout pageTitle="Home">
       <div className={`${styles.container} 'text-aling-center'`}>
