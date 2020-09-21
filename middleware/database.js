abcc8d32fe4314349c92d26b0fc0fb3ab28dd0a2
Erik Tagirov <erik@tagirov.ch>
2020-09-21 23:13:33 +0200

    Oupsie: Add database to middleware only once

--- middleware/database.js
+++ middleware/database.js
@@ -9,22 +9,17 @@ const dbName = "PvlseDev";
 const client = new MongoClient('mongodb://'+dbHostName+'/'+dbName, {
   auth: {
      user:"rob",
-     password: "Replace by pwd I've sent you earlier"
+     password: "pvlse_db_password_is_secure_because_its_ultra_long"
   },
   authMechanism: "SCRAM-SHA-256",
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-async function database(req, res, next) {
+export default async function database(req, res, next) {
   if (!client.isConnected()) await client.connect();
   req.dbClient = client;
   req.db = client.db(dbName);
   return next();
 }
 
-const middleware = nextConnect();
-
-middleware.use(database);
-
-export default middleware;
