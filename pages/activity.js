83a2c7c89ddf5a8e794e23362d448ba5e04d7c63
Roberto Moya <lastresmoyallc@gmail.com>
2020-09-23 17:53:21 -0600

    Create activity page, include navbar y footer

---
+++ pages/activity.js
@@ -0,0 +1,19 @@
+import { Row, Col } from 'reactstrap';
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+import CategoriesCard from '../components/Card/CategoriesCard'
+
+export default function activity () {
+    return (
+        <>
+            <NavBar />
+            <Row>
+                <Col>
+                    
+                </Col>
+            </Row>
+            <CategoriesCard />
+            <Footer />
+        </>
+    )
+}
