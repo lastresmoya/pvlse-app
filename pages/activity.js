0c393cb014af0f40054af17928c7ef715cbb35d1
Roberto Moya <lastresmoyallc@gmail.com>
2020-09-23 21:40:54 -0600

    Activity page, card

--- pages/activity.js
+++ pages/activity.js
@@ -81,19 +81,17 @@ export default function activity () {
                                     <h5 className="h5 text-white">
                                         Next session
                                     </h5>
-                                    <div class="card mb-3" style="max-width: 540px;">
-                                        <div class="row no-gutters">
-                                            <div class="col-md-1">
-                                            </div>
-                                            <div class="col-md-8">
-                                            <div class="card-body">
-                                                <h5 class="card-title">Card title</h5>
-                                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                            </div>
-                                            </div>
-                                        </div>
-                                        </div>
+                                    <div className="card mb-3">
+                                        <Row>
+                                            <Col>
+                                                <div className="card-body">
+                                                    <h5 className="d-inline card-title">Card title</h5>
+                                                    <p className="d-inline card-text">This </p>
+                                                    <p className="d-inline d-flex flex-row-reverse card-text"><small className="text-muted"> 3 mins ago</small></p>
+                                                </div>
+                                            </Col>
+                                        </Row>
+                                    </div>
                                 </div>
                             </Col>
                         </Row>
