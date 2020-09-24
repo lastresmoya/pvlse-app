1f7e9a8b348d4b720541e815f7c22ac63fd1a278
Roberto Moya <lastresmoyallc@gmail.com>
2020-09-23 18:19:39 -0600

    Merge master into activity/features branch

--- pages/activity.js
+++ pages/activity.js
@@ -1,18 +1,27 @@
 import { Row, Col } from 'reactstrap';
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-import CategoriesCard from '../components/Card/CategoriesCard'
+import '../styles/activity.module.scss';
 
 export default function activity () {
     return (
         <>
             <NavBar />
             <Row>
-                <Col>
-                    
+                <Col className="p-5" md="5">
+                        <Row>
+                            <Col className="card-shadow" md="6">
+                                <p> Hola</p>
+                            </Col>
+                            <Col className="bg-primary" md="6">
+                                <p> Hola</p>
+                            </Col>
+                        </Row>
+                </Col>        
+                <Col className="bg-info" md="7">
+                    <p>Hola</p>
                 </Col>
             </Row>
-            <CategoriesCard />
             <Footer />
         </>
     )
